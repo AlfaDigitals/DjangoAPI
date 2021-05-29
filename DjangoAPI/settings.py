6cbfcf41cb863414aec1b462c8c853203584246c
AlfaDigitals <85033973+AlfaDigitals@users.noreply.github.com>
2021-05-30 00:11:02 +0530

    commit

--- DjangoAPI/settings.py
+++ DjangoAPI/settings.py
@@ -17,6 +17,8 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
+heroku config:set DISABLE_COLLECTSTATIC=1
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,"media")
