079d611530754348dd1afcace76909f5cd0389cf
AlfaDigitals <85033973+AlfaDigitals@users.noreply.github.com>
2021-05-30 01:27:29 +0530

    commit

--- DjangoAPI/urls.py
+++ DjangoAPI/urls.py
@@ -25,7 +25,5 @@ from django.conf.urls import url
 from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^', include('EmployeeApp.urls')),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    url(r'^', include('EmployeeApp.urls')) 
 ]
