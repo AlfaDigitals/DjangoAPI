2e8831910d0e6d6410d3aedb94bd024e9fd93a9f
AlfaDigitals <85033973+AlfaDigitals@users.noreply.github.com>
2021-07-24 22:25:08 +0530

    push

--- DjangoAPI/settings.py
+++ DjangoAPI/settings.py
@@ -10,10 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
-from pathlib import Path
+
 import django_heroku
 import os
-
+from pathlib import Path
 
 
 
